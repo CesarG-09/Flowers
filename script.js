d9cc3de9546ea33e5f973dbeca7919ab2460c7e7
Cesar Garzón <156729288+CesarG-09@users.noreply.github.com>
2024-09-21 22:15:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+document.addEventListener('DOMContentLoaded', () => {
+  const audio = document.getElementById('background-audio');
+  audio.volume = 0.5; // Ajusta el volumen al 30%
+});
